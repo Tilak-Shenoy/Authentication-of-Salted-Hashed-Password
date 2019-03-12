27627929638f40461105b22baca0e751b16c89d2
Tilak Shenoy <tilaknshenoy@gmail.com>
2019-03-12 23:54:09 +0530

    Generated NDB

--- prefix.py
+++ prefix.py
@@ -1,30 +1,90 @@
 import numpy as np
-from utils import create_sequence_of_symbol, to_symbol,inv_permute
-# from permute import shuffle_forward, shuffle_backward
+from utils import create_sequence_of_symbol, to_symbol,inv_permute,comp, permute
+# from AES import encrypt
+import hashlib
+import random
 
 
 def prefix(s):
 	ndb=[]
-	perm_bits=np.random.permutation(s)
+	perm_bits=permute(s)
 	# perm_bits=shuffle_forward(s)
 	# print(perm_bits)
+	# print("Hellp")
 	m=len(perm_bits)
+	# j_ind=[i for i in range(len(s))]
+	# k_ind=[_ for _ in range(len(s))]
 
-	for i in range(m):
+	for i in range(m-1,1,-1):
 
 		x=list(create_sequence_of_symbol(m))
 		# print(x)
-		for j in range(1,i-1):
-			x[j]=to_symbol(perm_bits[j])
+		# for j in range(i-1):
+		# 	x[j]=to_symbol(perm_bits[j])
 
-		x[i]= to_symbol(~perm_bits[i])
-		for j in range(i+1,m):
-			x[j]='*'
+		# x[i]= to_symbol(comp(perm_bits[i]))
+		# for j in range(i+1,m):
+		# 	x[j]='*'
 
+		# print((x))
+		# ndb.append(inv_permute(x))
+		# ndb.append(x)
+
+		x[i]=to_symbol(comp(perm_bits[i]))
+		j=random.randint(0,i-1)
+		while True:
+			k=random.randint(0,i-1)
+			if j!=k:
+				break
+		x[j]=to_symbol(perm_bits[j])
+		x[k]=to_symbol(perm_bits[k])
 		# print(inv_permute(x))
+		print('\n\n')
 		ndb.append(inv_permute(x))
+
+	x=list(create_sequence_of_symbol(m))
+	x[0]=to_symbol(perm_bits[0])
+	x[1]=to_symbol(comp(perm_bits[1]))
+	j=random.randint(2,m-1)
+	x[j]='0'
+	ndb.append(inv_permute(x))
+	x[j]='1'
+	ndb.append(inv_permute(x))
+
+	x=list(create_sequence_of_symbol(m))
+	x[0]=to_symbol(comp(perm_bits[0]))
+	j=random.randint(1,m-1)
+	while True:
+		k=random.randint(1,m-1)
+		if j!=k:
+			break
+	x[j]='0'
+	x[k]='0'
+	ndb.append(inv_permute(x))
+	x[k]='1'
+	ndb.append(inv_permute(x))
+	x[k]='*'
+	# ndb.append(inv_permute(x))
+	while True:
+		k=random.randint(1,m-1)	
+		if j != k:
+			break
+	x[j]='1'
+	x[k]='0'
+	ndb.append(inv_permute(x))
+	x[k]='1'
+	ndb.append(inv_permute(x))
+
+
 
 	return ndb
 
-s=[2,3,0,4,1]
-print(prefix(s))
+
+def reg_phase(paswd):
+	#TODO: Compare with auth data table
+
+	hashed_pass=hashlib.sha256(paswd.encode('ascii')).hexdigest()
+	bin_hashed_pass="{0:b}".format(int(hashed_pass,16))
+	NDB=prefix(list(bin_hashed_pass))
+
+print(prefix([0,0,0,0]))
--- utils.py
+++ utils.py
@@ -11,19 +11,41 @@ def to_symbol(b):
 	else:
 		return str(b)
 
+
+def permute(arr):
+	table=[2,3,4,1]
+	arr2=[]
+	for i in range(len(arr)):
+		arr2.append(arr[table[i]-1])
+	return arr2
+
 # if element 4 is at position 3 in an array, then in reverse permutation, 
 # we insert 3 (position of element 4 in the array) in position 4 (element value).
 def inv_permute(arr) : 
-	arr2 = [0] *(len(arr)) 
-	  
+	# arr2 = [0] *(len(arr)) 
+	table = [4,1,2,3]
+	arr2=[]
+	for i in range(len(arr)):
+		arr2.append(arr[table[i]-1])
+	return arr2
+
 	# Inserting position at their 
 	# respective element in second array 
-	for i in range(0, len(arr)) : 
-		if arr[i] != '*':
-			# print(arr[i])
-			arr2[int(arr[i])%len(arr)] = i + 1
-		else:
-			arr2[i]='*'
+	# for i in range(len(arr)-1) : 
+	# 	if arr[i] != '*':
+	# 		# print(arr[i])
+	# 		arr2[int(arr[i])%len(arr)] = arr[i]
+	# 	else:
+	# 		arr2[i]='*'
+	# return arr2
 
-	return arr2
-	  
+def comp(b):
+	if b==0:
+		return 1
+	elif b==1:
+		return 0
+	else:
+		print('Error: Undefined bit value ',b)
+		return None
+
+# print(inv_permute([0,0,0,0]))	  
